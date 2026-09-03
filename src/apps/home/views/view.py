682690de46971da8efb33a0826b1eb5b4f43b3e3
zhangjiewu <zhangjiewu@aquanliang.com>
2026-09-03 21:25:46 +0800

    feat(home): 添加用户信息读取与创建功能
    - 引入 UserInfoModel 以便操作用户数据
    - 在 /info 路由中异步获取 id=1 的用户信息
    - 若用户存在，打印用户名
    - 异步创建一个新的用户数据，用户名和密码均为 test
    - 保持原有接口返回内容不变

--- src/apps/home/views/view.py
+++ src/apps/home/views/view.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+from src.models.User import UserInfoModel
 
 router = APIRouter()
 
 
 @router.get("/info")
 async def info():
+    user_info = await UserInfoModel.get_or_none(id=1)
+
+    if user_info:
+        print(user_info.username)
+
+    await UserInfoModel.create(username="test", password="test")
     return "info111"
